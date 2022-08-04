a145e5b3958a241a0e497d0c366028d4a2f5c97e
Господин Ведущий <sustaff@mail.ru>
2022-08-04 15:00:10 +0300

    Добавлена кнопка "Загрузить" и "Удалить"

--- TG_BOT/client_kb.py
+++ TG_BOT/client_kb.py
@@ -5,7 +5,11 @@ b2 = KeyboardButton("/Сумма_заказа")
 b3 = KeyboardButton("/О_нас")
 b4 = KeyboardButton("Поделиться номером", request_contact=True)
 b5 = KeyboardButton("Отправить,где я", request_location=True)
+b6 = KeyboardButton("/Меню")
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True).add(b6).add(b1).add(b2, b3)
 
-kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
-kb_client.row(b1,b2,b3).add(b4).add(b5)
+
+button_load = KeyboardButton('/Загрузить')
+button_delete = KeyboardButton('/Удалить')
+button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).add(button_delete)
