1176bc25d84b19591879526c1281fcb6d4df774d
Господин Ведущий <sustaff@mail.ru>
2022-08-04 14:59:03 +0300

    Добавлен пункт "Меню"

--- TG_BOT/main.py
+++ TG_BOT/main.py
@@ -1,21 +1,30 @@
 import time
 
+import admin
+
 import handlers as h
 from aiogram import Bot
 from aiogram import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
+from data_base import sqlite_db
 
-TOKEN = "5320843943:AAGc_40shVPFQeYN5s5YTb8cm19HVJ8qzxQ"
+
+storage = MemoryStorage()
+
+TOKEN = "5394717777:AAE22w9qhC-a8dW7COJdWxlGLJDyYnRGJhE"
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+admin.register_handlers_admin(dp)
 
 
 async def on_startup(_):
     print("Идёт подключение к серверу...")
-    time.sleep(2)
+
     print("Бот вышел в онлайн.")
-    time.sleep(2)
+
     print("Готов к работе!")
+    sqlite_db.sql_start()
 
 
 dp.register_message_handler(h.command_start, commands=["start"])
@@ -23,6 +32,7 @@ dp.register_message_handler(h.command_help, commands=["help"])
 dp.register_message_handler(h.command_open_time, commands=["Режим_работы"])
 dp.register_message_handler(h.command_summa_zakaza, commands=["Сумма_заказа"])
 dp.register_message_handler(h.command_o_nas, commands=["О_нас"])
+dp.register_message_handler(h.menu, commands=["Меню"])
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
